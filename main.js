bcc4374158cb5da5932ddbe115356eb83b958bd5
Here <khere220@gmail.com>
2021-02-26 23:07:54 +0900

    Week2 Done

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 'use strict'
 
-const navbar = document.querySelector('#navbar')
-const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
-    if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
-});
-
 // Show "arrow up" button when scrolling down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
 const arrow = document.querySelector('.arrow');
 document.addEventListener('scroll', () => {
     if (window.scrollY > homeHeight / 2) {
@@ -27,7 +16,11 @@ document.addEventListener('scroll', () => {
     }
 });
 
+arrow.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth'});
-}
+};
